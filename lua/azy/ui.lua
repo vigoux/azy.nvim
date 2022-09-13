893d5734de8179cf882c7a78d794c1a3847a74ba
Thomas Vigouroux <thomas.vigouroux@univ-grenoble-alpes.fr>
2022-09-13 07:42:56 +0200

    feat(ui): allow navigation and selection of element

--- lua/azy/ui.lua
+++ lua/azy/ui.lua
@@ -1,6 +1,11 @@
 local fzy = require('fzy-lua-native')
 local hl_ns = vim.api.nvim_create_namespace('azy')
 
+local AzyLine = {}
+
+
+
+
 local AzyUi = {}
 
 
@@ -11,8 +16,28 @@ local AzyUi = {}
 
 
 
-function AzyUi.create(content)
-   AzyUi._source_lines = content
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function AzyUi.create(content, callback)
+   AzyUi._callback = callback or function(i) vim.notify(i) end
+   AzyUi._hl_positions = {}
+   AzyUi._source_content = content
+   AzyUi._source_lines = vim.tbl_map(function(e)
+      return { content = e, selected = false }
+   end, content)
    AzyUi._input_buf = vim.api.nvim_create_buf(false, true)
    AzyUi._output_buf = vim.api.nvim_create_buf(false, true)
 
@@ -23,7 +48,7 @@ function AzyUi.create(content)
    local display_height = 20
    local input_row = lines - (display_height + 2)
 
-   local iwin = vim.api.nvim_open_win(AzyUi._input_buf, true, {
+   AzyUi._input_win = vim.api.nvim_open_win(AzyUi._input_buf, true, {
       relative = 'editor',
       anchor = 'NW',
       width = columns,
@@ -35,8 +60,8 @@ function AzyUi.create(content)
       border = 'none',
    })
 
-   vim.api.nvim_win_set_option(iwin, 'winblend', 0)
-   local owin = vim.api.nvim_open_win(AzyUi._output_buf, false, {
+   vim.api.nvim_win_set_option(AzyUi._input_win, 'winblend', 0)
+   AzyUi._output_win = vim.api.nvim_open_win(AzyUi._output_buf, false, {
       relative = 'editor',
       anchor = 'NW',
       width = columns,
@@ -48,7 +73,7 @@ function AzyUi.create(content)
       border = 'none',
       noautocmd = true,
    })
-   vim.api.nvim_win_set_option(owin, 'winblend', 0)
+   vim.api.nvim_win_set_option(AzyUi._output_win, 'winblend', 0)
 
 
    vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
@@ -59,42 +84,125 @@ function AzyUi.create(content)
 
    vim.api.nvim_create_autocmd({ "BufUnload", "BufDelete", "BufWipeout", "BufLeave" }, {
       buffer = AzyUi._input_buf,
-      callback = function()
-         vim.api.nvim_win_close(iwin, true)
-         vim.api.nvim_win_close(owin, true)
-      end,
+      callback = AzyUi.close,
    })
+
+
+   vim.keymap.set({ "n", "i" }, "<Down>", AzyUi.next, { buffer = AzyUi._input_buf })
+   vim.keymap.set({ "n", "i" }, "<Up>", AzyUi.prev, { buffer = AzyUi._input_buf })
+   vim.keymap.set({ "n", "i" }, "<CR>", AzyUi.confirm, { buffer = AzyUi._input_buf })
 
    AzyUi._update_output_buf()
 end
 
+function AzyUi._pick_next(inlines)
+   local found_selected = false
+
+   for i, sline in ipairs(inlines) do
+      if found_selected then
+         sline.selected = true
+         AzyUi._update_output_buf()
+         return
+      end
+      if sline.selected and i < #inlines then
+         found_selected = true
+         sline.selected = false
+      end
+   end
+
+
+   AzyUi._redraw()
+end
+
+function AzyUi.confirm()
+   local selected = vim.tbl_filter(function(e) return e.selected end, AzyUi._current_lines)[1]
+   AzyUi._callback(selected.content)
+   AzyUi.close()
+end
+
+function AzyUi.next()
+   AzyUi._pick_next(AzyUi._current_lines)
+end
+
+function AzyUi.prev()
+   local tmp = {}
+   for _, l in ipairs(AzyUi._current_lines) do
+      table.insert(tmp, 1, l)
+   end
+   AzyUi._pick_next(tmp)
+end
+
+function AzyUi.close()
+   if vim.api.nvim_win_is_valid(AzyUi._input_win) then
+      vim.api.nvim_win_close(AzyUi._input_win, true)
+   end
+   if vim.api.nvim_win_is_valid(AzyUi._output_win) then
+      vim.api.nvim_win_close(AzyUi._output_win, true)
+   end
+end
+
 function AzyUi._update_output_buf()
    local iline = vim.api.nvim_buf_get_lines(AzyUi._input_buf, 0, -1, true)[1]
-
    if #iline > 0 then
-      local result = fzy.filter(iline, AzyUi._source_lines, false)
+      local result = fzy.filter(iline, AzyUi._source_content, false)
 
       table.sort(result, function(a, b)
          return a[3] < b[3]
       end)
 
-      local outlines = {}
-      local hl_lines = {}
+      AzyUi._current_lines = {}
+      AzyUi._hl_positions = {}
+
       for _, r in ipairs(result) do
-         table.insert(outlines, r[1])
-         table.insert(hl_lines, r[2])
+         local source_line = vim.tbl_filter(function(e)
+            return e.content == r[1]
+         end, AzyUi._source_lines)[1]
+         table.insert(AzyUi._current_lines, source_line)
+         table.insert(AzyUi._hl_positions, r[2])
       end
 
-      vim.api.nvim_buf_set_lines(AzyUi._output_buf, 0, -1, true, outlines)
-
-      for i, hls in ipairs(hl_lines) do
-         for _, hl in ipairs(hls) do
-            print(i, hl)
-            vim.api.nvim_buf_add_highlight(AzyUi._output_buf, hl_ns, "Error", i - 1, hl - 1, hl)
-         end
-      end
    else
-      vim.api.nvim_buf_set_lines(AzyUi._output_buf, 0, -1, true, AzyUi._source_lines)
+      AzyUi._current_lines = AzyUi._source_lines
+      AzyUi._hl_positions = {}
+   end
+
+
+   local selected_index = 0
+   for i, sline in ipairs(AzyUi._current_lines) do
+      if sline.selected and selected_index == 0 then
+         selected_index = i
+      end
+   end
+
+
+   if selected_index == 0 and #AzyUi._current_lines > 0 then
+      for _, sline in ipairs(AzyUi._source_lines) do
+         sline.selected = false
+      end
+      AzyUi._current_lines[1].selected = true
+   end
+
+   AzyUi._redraw()
+end
+
+function AzyUi._redraw()
+   local lines_to_draw = {}
+   vim.api.nvim_buf_clear_namespace(AzyUi._output_buf, hl_ns, 0, -1)
+   for _, line in ipairs(AzyUi._current_lines) do
+      if line.selected then
+         table.insert(lines_to_draw, "> " .. line.content)
+      else
+         table.insert(lines_to_draw, "  " .. line.content)
+      end
+   end
+
+   vim.api.nvim_buf_set_lines(AzyUi._output_buf, 0, -1, true, lines_to_draw)
+
+   for i, hls in ipairs(AzyUi._hl_positions) do
+      for _, hl in ipairs(hls) do
+
+         vim.api.nvim_buf_add_highlight(AzyUi._output_buf, hl_ns, "Error", i - 1, hl + 1, hl + 2)
+      end
    end
 end
 
