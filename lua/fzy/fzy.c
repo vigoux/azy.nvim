4ba88c5de8668707debb652c25ea1e499b5a3190
Thomas Vigouroux <thomas.vigouroux@univ-grenoble-alpes.fr>
2022-09-15 08:03:14 +0200

    fix: hold references to all strings used in the choices object

--- lua/fzy/fzy.c
+++ lua/fzy/fzy.c
@@ -9,75 +9,96 @@
 
 #define CHOICES_META "choices_metatable"
 
-static choices_t * fzy_choices_check(lua_State *L, int index)
+typedef struct {
+  // Choices struct
+  choices_t choices;
+
+  // Reference to table the stores references to the strings used by the choices object
+  int ref;
+} choices_ud_t;
+
+static choices_ud_t * fzy_choices_check(lua_State *L, int index)
 {
-  return (choices_t *)luaL_checkudata(L, index, CHOICES_META);
+  return (choices_ud_t *)luaL_checkudata(L, index, CHOICES_META);
 }
 
 static int fzy_choices_gc(lua_State *L)
 {
-  choices_t * tofree = fzy_choices_check(L, -1);
-  choices_destroy(tofree);
+  choices_ud_t * tofree = fzy_choices_check(L, -1);
+  choices_destroy(&tofree->choices);
+
+  // Remove reference to reference table
+  luaL_unref(L, LUA_REGISTRYINDEX, tofree->ref);
   return 0;
 }
 
+// Creates a new choices object
+//
+// Upon creation, also create the reference table.
 static int fzy_choices_create(lua_State *L)
 {
-  choices_t * new = (choices_t *)lua_newuserdata(L, sizeof(choices_t));
+  choices_ud_t * new = (choices_ud_t *)lua_newuserdata(L, sizeof(choices_ud_t));
   // TODO(vigoux): Make the number of workers configurable ?
-  choices_init(new, 0);
+  choices_init(&new->choices, 0);
 
   // Add the metatable to the stack
   luaL_getmetatable(L, CHOICES_META);
 
   // Set the metatable on the userdata
   lua_setmetatable(L, -2);
+
+  // Create the reference table
+  lua_createtable(L, 1, 0);
+  new->ref = luaL_ref(L, LUA_REGISTRYINDEX);
   return 1;
 }
 
 static int fzy_choices_populate(lua_State *L)
 {
-  choices_t * c = fzy_choices_check(L, 1);
+  choices_ud_t * c = fzy_choices_check(L, 1);
   luaL_checktype(L, 2, LUA_TTABLE);
+
+  // Prepare the stack for addition to the reference table
+  lua_rawgeti(L, LUA_REGISTRYINDEX, c->ref); // [reftbl]
 
   // Now iterate over the table and add to the choices
   size_t nelems = lua_objlen(L, 2);
   for (size_t i = 0; i < nelems; i++) {
-    lua_rawgeti(L, 2, i + 1); // [elem]
+    lua_rawgeti(L, 2, i + 1); // [reftbl, elem]
 
-    // FIXME: there may be some problems here with the lifetime of the strings
     const char * choice = luaL_checkstring(L, -1);
-    choices_add(c, choice);
-    lua_pop(L, 1); // []
+    choices_add(&c->choices, choice);
+    luaL_ref(L, -2); // [reftbl]
   }
+  lua_pop(L, 1);
 
-  return 1;
+  return 0;
 }
 
 static int fzy_choices_available(lua_State *L)
 {
-  choices_t * c = fzy_choices_check(L, 1);
+  choices_ud_t * c = fzy_choices_check(L, 1);
 
-  lua_pushinteger(L, choices_available(c));
+  lua_pushinteger(L, choices_available(&c->choices));
   return 1;
 }
 
 static int fzy_choices_search(lua_State *L)
 {
-  choices_t * c = fzy_choices_check(L, 1);
+  choices_ud_t * c = fzy_choices_check(L, 1);
   const char * needle = luaL_checkstring(L, 2);
 
-  choices_search(c, needle);
+  choices_search(&c->choices, needle);
 
   // Now create the result table
-  lua_createtable(L, choices_available(c), 0); // [ret]
-  for (size_t i = 0; i < choices_available(c); i++) {
+  lua_createtable(L, choices_available(&c->choices), 0); // [ret]
+  for (size_t i = 0; i < choices_available(&c->choices); i++) {
     lua_createtable(L, 2, 0); // [ret, elem]
                               //
-    lua_pushstring(L, choices_get(c, i)); // [ret, elem, str]
+    lua_pushstring(L, choices_get(&c->choices, i)); // [ret, elem, str]
     lua_rawseti(L, -2, 1); // [ret, elem]
 
-    lua_pushnumber(L, choices_getscore(c, i)); // [ret, elem, score]
+    lua_pushnumber(L, choices_getscore(&c->choices, i)); // [ret, elem, score]
     lua_rawseti(L, -2, 2); // [ret, elem]
 
     lua_rawseti(L, -2, i+1); // [ret]
@@ -85,7 +106,7 @@ static int fzy_choices_search(lua_State *L)
   return 1;
 }
 
-static void push_selected(lua_State *L, choices_t * c)
+static void push_selected(lua_State *L, choices_t* c)
 {
   if (c->available) {
     lua_pushstring(L, choices_get(c, c->selection));
@@ -96,27 +117,27 @@ static void push_selected(lua_State *L, choices_t * c)
 
 static int fzy_choices_selected(lua_State *L)
 {
-  choices_t * c = fzy_choices_check(L, 1);
+  choices_ud_t * c = fzy_choices_check(L, 1);
 
-  push_selected(L, c);
+  push_selected(L, &c->choices);
   return 1;
 }
 
 static int fzy_choices_next(lua_State *L)
 {
-  choices_t * c = fzy_choices_check(L, 1);
-  choices_next(c);
+  choices_ud_t * c = fzy_choices_check(L, 1);
+  choices_next(&c->choices);
 
-  push_selected(L, c);
+  push_selected(L, &c->choices);
   return 1;
 }
 
 static int fzy_choices_prev(lua_State *L)
 {
-  choices_t * c = fzy_choices_check(L, 1);
-  choices_prev(c);
+  choices_ud_t * c = fzy_choices_check(L, 1);
+  choices_prev(&c->choices);
 
-  push_selected(L, c);
+  push_selected(L, &c->choices);
   return 1;
 }
 
