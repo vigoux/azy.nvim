8b498e8d451647b800ad1d3dc5309347153737d5
Thomas Vigouroux <thomas.vigouroux@univ-grenoble-alpes.fr>
2022-09-21 08:21:50 +0200

    test: add tests for incremental addition and cursor movement

--- tests/fzy.lua
+++ tests/fzy.lua
@@ -1,7 +1,8 @@
 local fzy = require'fzy'
-local say = require'say'
 require 'busted.runner' { output = 'TAP', shuffle = true }
 
+-- Setup some helpers to check the elemenst
+local say = require'say'
 local function ordered_elements(state, arguments)
   if #arguments ~= 2 then
     return false
@@ -10,10 +11,12 @@ local function ordered_elements(state, arguments)
   local source = arguments[1]
   local check = arguments[2]
 
-  if type(source) ~= 'table' or type(check) ~= 'table' then
+  if type(source) ~= 'userdata' or type(check) ~= 'table' then
     print "type"
     return false
   end
+
+  source = source:elements()
 
   if #source ~= #check then
     print "len"
@@ -34,6 +37,7 @@ say:set("assertion.has_correct_elements.positive", "Expected %s \nto be ordered 
 say:set("assertion.has_correct_elements.negative", "Expected %s \nto not be ordered like: %s")
 assert:register("assertion", "has_correct_elements", ordered_elements, "assertion.has_correct_elements.positive", "assertion.has_correct_elements.negative")
 
+-- Actual tests
 describe('fzy native library', function()
 
   it('can create choices object', function()
@@ -42,16 +46,81 @@ describe('fzy native library', function()
     assert.is_not_nil(c)
   end)
 
-  it('filters results', function()
-    local c = fzy.create()
+  describe('choices object', function()
+    local c
 
-    c:add { "babar", "baz", "foo" }
+    before_each(function()
+      c = fzy.create()
+      c:add { "babar", "baz", "foo" }
+    end)
 
-    -- Ensure that behavior of no avaiable items on the start
-    assert.is.equal(0, c:available())
+    it('filters results', function()
+      -- Ensure that behavior of no avaiable items on the start
+      assert.is.equal(0, c:available())
 
-    c:search "a"
+      c:search "a"
 
-    assert.has_correct_elements(c:elements(), { "baz", "babar" })
+      assert.has_correct_elements(c, { "baz", "babar" })
+    end)
+
+    it('allows to filter incrementally', function()
+      c:search "a"
+
+      c:add_incremental { "abar" }
+
+      assert.has_correct_elements(c, { "abar", "baz", "babar" })
+    end)
+
+    it('correctly filters with new queries', function()
+      c:search "a"
+      assert.has_correct_elements(c, { "baz", "babar" })
+      c:search "ab"
+      assert.has_correct_elements(c, { "babar" })
+    end)
+  end)
+
+  describe('cursor navigation', function()
+    local c
+
+    before_each(function()
+      c = fzy.create()
+      c:add { "babar", "baz", "foo" }
+    end)
+
+    it('is not set on start', function()
+      assert.is_nil(c:selected())
+    end)
+
+    it('is set after search', function()
+      c:search "a"
+      assert.is.equal("baz", c:selected())
+    end)
+
+    it('correctly moves', function()
+      c:search "a"
+
+      assert.is.equal("baz", c:selected())
+      assert.is.equal("babar", c:next())
+      assert.is.equal("baz", c:prev())
+    end)
+
+    it('wraps around', function()
+      c:search "a"
+
+      assert.is.equal("baz", c:selected())
+      assert.is.equal("babar", c:next())
+      assert.is.equal("baz", c:next())
+    end)
+
+    it('moves when incrementally adding results', function()
+      c:search "a"
+
+      assert.is.equal("baz", c:selected())
+
+      c:add_incremental { "abar" }
+
+      assert.is.equal("baz", c:selected())
+      assert.is.equal("abar", c:prev())
+    end)
   end)
 end)
